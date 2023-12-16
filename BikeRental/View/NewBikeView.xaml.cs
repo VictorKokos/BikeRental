6407de109c1b44d4166db4d23f638ded75ee2a88
VictorKokos <Travel.ru15@gmail.com>
2023-12-16 21:57:11 +0300

    possibility to add bike

--- BikeRental/View/NewBikeView.xaml.cs
+++ BikeRental/View/NewBikeView.xaml.cs
@@ -24,6 +24,6 @@ namespace BikeRental
             InitializeComponent();
             DataContext = new NewBikeViewModel(bikeService);
         }
-
     }
+
 }
--- BikeRental/ViewModel/NewBikeViewModel.cs
+++ BikeRental/ViewModel/NewBikeViewModel.cs
@@ -3,43 +3,50 @@ using MVVM;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-public class NewBikeViewModel : INotifyPropertyChanged
+namespace BikeRental
 {
-    private BikeService _bikeService;
-    private Bike _newBike;
-
-    public NewBikeViewModel(BikeService bikeService)
+    public class NewBikeViewModel : INotifyPropertyChanged
     {
-        _bikeService = bikeService;
-        _newBike = new Bike();
-    }
+        private BikeService _bikeService;
+        private Bike _newBike;
 
-    public Bike NewBike
-    {
-        get { return _newBike; }
-        set
+        public NewBikeViewModel(BikeService bikeService)
         {
-            _newBike = value;
-            OnPropertyChanged("NewBike");
+            _bikeService = bikeService;
+            _newBike = new Bike();
         }
-    }
-
-    private RelayCommand _saveCommand;
-    public RelayCommand SaveCommand
-    {
-        get
+        public NewBikeViewModel( )
         {
-            return _saveCommand ??
-                (_saveCommand = new RelayCommand(obj =>
-                {
-                    _bikeService.AddBike(NewBike);
-                }));
+          
         }
-    }
 
-    public event PropertyChangedEventHandler PropertyChanged;
-    public void OnPropertyChanged([CallerMemberName] string prop = "")
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        public Bike NewBike
+        {
+            get { return _newBike; }
+            set
+            {
+                _newBike = value;
+                OnPropertyChanged("NewBike");
+            }
+        }
+
+        private RelayCommand _saveCommand;
+        public RelayCommand SaveCommand
+        {
+            get
+            {
+                return _saveCommand ??
+                    (_saveCommand = new RelayCommand(obj =>
+                    {
+                        _bikeService.AddBike(NewBike);
+                    }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
